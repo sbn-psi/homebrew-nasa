4acfcaadacc8c0781254ce0f3192138f753f87a4
Jesse Stone <jstone@psi.edu>
2018-04-16 15:54:27 -0700

    added cask for FV

---
+++ Casks/fv.rb
@@ -0,0 +1,10 @@
+cask 'fv' do
+  version '5.4'
+  sha256 '85eaf288aea346176abdfdd262f2e9c04d46494844e38731aad64a86907c8f2b'
+
+  url 'https://heasarc.gsfc.nasa.gov/FTP/software/lheasoft/fv/fv5.4_MacOS10.x.dmg'
+  name 'fv FITS File Viewer'
+  homepage 'https://heasarc.gsfc.nasa.gov/ftools/fv/'
+
+  app 'fv.app'
+end
