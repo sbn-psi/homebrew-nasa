3479040f3fc96821458838856a2ed3e8fdcdb03b
Jesse Stone <jstone@psi.edu>
2019-02-20 10:07:32 -0700

    updated for 3.18

--- Casks/nasaview.rb
+++ Casks/nasaview.rb
@@ -1,10 +1,10 @@
 cask 'nasaview' do
-  version '3.17.0'
-  sha256 '513483818c3709d8f6cddde05a0a505aa086ce4f52a55e1ae9a4589b8daac1f5'
+  version '3.18.0'
+  sha256 '90a98d211e87b17328853d325378478844fca69e23d6210d51601a89c30a9794'
 
   url "ftp://pds.nasa.gov/pub/toplevel/tools/bin/nasaview-#{version}_mac_intel.tar.gz"
   name 'NASAView'
   homepage 'https://pds.jpl.nasa.gov/tools/nasa-view.shtml'
 
-  app 'nasaview-3.17.0_mac_intel/NASAView.app'
+  app 'nasaview-#{version}_mac_intel/NASAView.app'
 end
